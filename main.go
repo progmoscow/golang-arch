01d61a98f29419568034dbe27f5d8686a29efb67
neon <prog.moscow@gmail.com>
2020-11-12 11:18:36 +0300

    sets up server

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"net/http"
 )
 
 type person struct {
@@ -12,24 +10,36 @@ type person struct {
 }
 
 func main() {
-	p1 := person{
-		First: "Tom",
-	}
-	p2 := person{
-		First: "James",
-	}
-	xp := []person{p1, p2}
+	// p1 := person{
+	// 	First: "Tom",
+	// }
+	// p2 := person{
+	// 	First: "James",
+	// }
+	// xp := []person{p1, p2}
 
-	bs, err := json.Marshal(xp)
+	// bs, err := json.Marshal(xp)
 
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("PRINT JSON", string(bs))
-	xp2 := []person{}
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("back into a Go data structure", xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("PRINT JSON", string(bs))
+	// xp2 := []person{}
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("back into a Go data structure", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8000", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
 }
